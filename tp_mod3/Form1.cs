6e1e6dea2a294f4aea264c9b4c88b7e1a013cfe4
purianagatha <purianagatha@gmail.com>
2024-03-04 01:08:47 +0700

    create new form application

---
+++ tp_mod3/Form1.cs
@@ -0,0 +1,31 @@
+namespace tp_mod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nama = textBox1.Text;
+            label1.Text = "Halo " + nama;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
